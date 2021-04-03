43f4d69ed1a260ba9a993964f15d704f0ac637ad
Timur <timur.shevtsov1984@gmail.com>
2021-04-03 13:09:19 +0200

    test Envybox app:    add Vue and basic components in project.

---
+++ resources/js/components/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="d-flex align-items-center justify-content-center flex-column">
+      <messages />
+      <form-feedback />
+  </div>
+</template>
+
+<script>
+  import Messages from './Messages'
+  import FormFeedback from './FormFeedback'
+  
+  export default {
+    name: 'App',
+    components: {Messages, FormFeedback}
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/FormFeedback.vue
@@ -0,0 +1,31 @@
+<template>
+    <form>
+        <div class="form-group">
+            <label for="name">Your Name</label>
+            <input type="text" class="form-control" id="name" aria-describedby="nameHelp">
+            <small id="nameHelp" class="form-text text-muted">Required</small>
+        </div>
+        <div class="form-group">
+            <label for="phone">Password</label>
+            <input type="text" class="form-control" id="phone" aria-describedby="phoneHelp">
+            <small id="phoneHelp" class="form-text text-muted">+34 999 999 999</small>
+        </div>
+        <div class="form-group">
+            <label for="message">Textarea</label>
+            <textarea class="form-control" name="message" aria-describedby="messageHelp" id="message"
+                      placeholder="Message ..." required></textarea>
+            <small id="messageHelp" class="form-text text-muted">Required</small>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</template>
+
+<script>
+  export default {
+    name: 'FormFeedback'
+  }
+</script>
+
+<style scoped>
+
+</style>
